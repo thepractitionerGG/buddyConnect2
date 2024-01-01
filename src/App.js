f472632ace9b81f76fcb9af04e0b3446ea19876b
Deep Bansal <thepractitioner.gg@gmail.com>
2024-01-01 11:01:17 +0530

    main project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // the following className can access attruibute of tail wind like h screen,bg and flex
+    <div className="h-screen bg-blue-400 flex justify-center items-center" >  
+      <h1 className="text-3xl text-white"> BUDDY CONNECT  </h1>
     </div>
   );
 }
