9ca19ce69fbb753222fb9905df885c25bd34dc1c
sreedharashwin <s223955271@deakin.edu.au>
2024-01-24 15:52:40 +1100

    test

---
+++ test/test.js
@@ -0,0 +1,19 @@
+var expect  = import("chai").expect;
+var request = import("request");
+
+describe("Login Page", function() {
+    var url = "http://localhost:3000/login";
+    it("returns status 200 to check if api works", function(done) {
+        request(url, function(error, response, body) {
+            expect(response.statusCode).to.equal(200);
+            done()
+          });
+    });
+    it("returns statusCode key in body to check if api give right result should be 200", function(done) {
+        request(url, function(error, response, body) {
+            body = JSON.parse(body)
+            expect(body.statusCode).to.equal(200);
+            done()
+          });
+    });
+}); 
