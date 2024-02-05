ab125982c98771df394abe78d626215f954420a5
Vikram <thepractitioner.gg@gmail.com>
2024-02-06 00:22:42 +1100

    user online status and api calls and bug fixes

--- Client/src/Pages/Home/components/UserSearch.js
+++ Client/src/Pages/Home/components/UserSearch.js
@@ -1,24 +1,19 @@
-import React from 'react'
-import {RiFileSearchLine , RiSearchLine} from "@remixicon/react"
+import React from "react";
 
-const UserSearch = ({searchKey , setSearchKey}) => {
+function UserSearch({searchKey , setSearchKey}) {
+  
   return (
-    <div>
     <div className="relative">
       <input
         type="text"
-        placeholder="Search new people / chats"
+        placeholder="Search users / chats"
         className="rounded-xl w-full border-gray-300 pl-10 text-gray-500 h-14"
         value={searchKey}
         onChange={(e) => setSearchKey(e.target.value)}
       />
-      <RiSearchLine
-      className='absolute top-4 left-4 text-gray-500'
-      />
-      {/* <i className="ri-search-line absolute top-4 left-4 text-gray-500"></i> */}
+      <i className="ri-search-line absolute top-4 left-4 text-gray-500"></i>
     </div>
-    </div>
-  )
+  );
 }
 
-export default UserSearch
+export default UserSearch;
--- Server/Routes/usersRoute.js
+++ Server/Routes/usersRoute.js
@@ -1,130 +1,152 @@
-const UserModel = require("../models/userModel");
+const User = require("../models/userModel");
 const router = require("express").Router();
-const bycrypt = require("bcryptjs");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const authMiddleware = require("../middleware/authMiddleware");
-
+const authMiddleware = require("../middlewares/authMiddleware");
+const cloudinary = require("../cloudinary");
 
 // user registration
 
 router.post("/register", async (req, res) => {
-    try {
-        //check if user alreday exist
-        const user = await UserModel.findOne({ email: req.body.email });
-        if (user) {
-            return res.status(200).send({
-                success: false,
-                message: "User ALready Exists",
-            });
-        }
-
-        // create new user
-        const hashedPassword = await bycrypt.hash(req.body.password, 10);
-        req.body.password = hashedPassword;
-        const newUser = new UserModel(req.body);
-        await newUser.save();
-        res.status(200).send({
-            success: true,
-            message: "New user created successfully!!",
-        });
+  try {
+    // check if user already exists
+    const user = await User.findOne({ email: req.body.email });
+    if (user) {
+      return res.send({
+        success: false,
+        message: "User already exists",
+      });
     }
 
-    catch (error) {
-        res.send({
-            message: error.message,
-            success: false,
-        });
-    }
+    // create new user
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    req.body.password = hashedPassword;
+    const newUser = new User(req.body);
+    await newUser.save();
+    res.send({
+      success: true,
+      message: "User created successfully",
+    });
+  } catch (error) {
+    res.send({
+      message: error.message,
+      success: false,
+    });
+  }
 });
 
 // user login
 
 router.post("/login", async (req, res) => {
-    try {
-        //check if user exist 
-        const user = await UserModel.findOne({
-            email: req.body.email
-        });
-      
-        if (!user) {
-            return res.send({
-                success: false,
-                message: "User does not exists!"
-            });
-        }
+  try {
+    // check if user exists
 
-        const validPassword = await bycrypt.compare(req.body.password, user.password);
-
-        if (!validPassword) {
-            return res.send({
-                success: false,
-                message: "Invalid Password"
-            });
-        }
-
-        // create and assign a token
-        const token = jwt.sign({ userid: user._id }, process.env.JWT_SECRET,{expiresIn:"1d",});
-        res.send({
-            success: true,
-            message: "User logged in successfully",
-            data: token
-        });
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      return res.send({
+        success: false,
+        message: "User does not exist",
+      });
     }
-    catch (error) {
-        res.send({
-            message: error.message,
-            success: false,
-        })
-    };
+
+    // check if password is correct
+
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!validPassword) {
+      return res.send({
+        success: false,
+        message: "Invalid password",
+      });
+    }
+
+    // create and assign a token
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+    res.send({
+      success: true,
+      message: "User logged in successfully",
+      data: token,
+    });
+  } catch (error) {
+    res.send({
+      message: error.message,
+      success: false,
+    });
+  }
 });
 
 // get current user
 
-router.get("/get-current-user",authMiddleware,async(req,res)=>{
-    try {
-        let user = req.body.user
-        if(user){
-            res.send({
-                success:true,
-                message:"User Fetched Successfully",
-                data:user,
-              });
-        }else{
-            throw Error('Not Found')
-        }
-    } catch (error) {
-        res.send({
-            message:error.message,
-            success:false,
-        });
-    }
-})
+router.get("/get-current-user", authMiddleware, async (req, res) => {
+  try {
+    const user = await User.findOne({ _id: req.body.userId });
+    res.send({
+      success: true,
+      message: "User fetched successfully",
+      data: user,
+    });
+  } catch (error) {
+    res.send({
+      message: error.message,
+      success: false,
+    });
+  }
+});
 
+// get all users except current user
 
-//GET All User
+router.get("/get-all-users", authMiddleware, async (req, res) => {
+  try {
+    const allUsers = await User.find({ _id: { $ne: req.body.userId } });
+    res.send({
+      success: true,
+      message: "Users fetched successfully",
+      data: allUsers,
+    });
+  } catch (error) {
+    res.send({
+      message: error.message,
+      success: false,
+    });
+  }
+});
 
-router.get('/allUsers' , authMiddleware , async (req , res)=>{
-    try {
-        let currUser =  req.body.user
-        let allUsers =  await UserModel.find({_id:{$ne:currUser._id}})
-        if(allUsers.length>0){
-            return res.status(200).send({
-                success:true,
-                data:allUsers
-            })
-        }else{
-            return res.status(404).send({
-                success:false,
-                message:'No user found'
-            })
-        }
-    } catch (error) {
-        console.log('Error in GetAll Users', error);
-        res.status(500).send({
-            success:false,
-            message:'Some error Occured'
-        })
-    }
-})
+// update user profile picture
+
+router.post("/update-profile-picture", authMiddleware, async (req, res) => {
+  try {
+    const image = req.body.image;
+
+    // upload image to cloudinary and get url
+
+    const uploadedImage = await cloudinary.uploader.upload(image, {
+      folder: "ksr",
+    });
+
+    // update user profile picture
+
+    const user = await User.findOneAndUpdate(
+      { _id: req.body.userId },
+      { profilePic: uploadedImage.secure_url },
+      { new: true }
+    );
+
+    res.send({
+      success: true,
+      message: "Profile picture updated successfully",
+      data: user,
+    });
+  } catch (error) {
+    res.send({
+      message: error.message,
+      success: false,
+    });
+  }
+});
 
 module.exports = router;
